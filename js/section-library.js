de07341293cf537bc2743e8ba100a3a032e88141
Mahmoud Zayed <mahmoudeid2050@gmail.com>
2020-12-26 22:59:34 -0500

    Init section library js.

---
+++ js/section-library.js
@@ -0,0 +1,38 @@
+/**
+ * @file
+ * Behaviors Section Library general scripts.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  "use strict";
+
+  var layoutBuilderSectionLibraryFiltered = false;
+
+  Drupal.behaviors.sctionLibrary = {
+    attach: function (context) {
+      var $filterLinks = $('.js-layout-builder-section-library-link');
+
+      var filterSectionLibraryList = function filterSectionLibraryList(e) {
+        var query = $(e.target).val().toLowerCase();
+
+        var toggleSectionLibraryEntry = function toggleSectionLibraryEntry(index, link) {
+          var $link = $(link);
+          var textMatch = $link.text().toLowerCase().indexOf(query) !== -1;
+          $link.toggle(textMatch);
+        };
+
+        if (query.length >= 2) {
+          $filterLinks.each(toggleSectionLibraryEntry);
+          layoutBuilderSectionLibraryFiltered = true;
+        } else if (layoutBuilderSectionLibraryFiltered) {
+          layoutBuilderSectionLibraryFiltered = false;
+          $filterLinks.show();
+          Drupal.announce(Drupal.t('All available sections are listed.'));
+        }
+      };
+
+      $('input.js-layout-builder-section-library-filter', context).once('js-layout-builder-section-library-filter').on('keyup', Drupal.debounce(filterSectionLibraryList, 200));
+    }
+  };
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
